80683d5903748ad35971580bbf637253998bb4b7
chrismre <chrismre@cs.stanford.edu>
2018-01-24 14:45:19 -0800

    Fix extra chunk size

--- pytorch/pytorch_hyperbolic.py
+++ pytorch/pytorch_hyperbolic.py
@@ -175,9 +175,10 @@ def build_distance(G, scale, num_workers=None):
         logging.info(f"\t Creating {nChunks} of size {chunk_size} and an extra chunk of size {extra_chunk_size}")
 
         chunks     = [ list(range(k*chunk_size, (k+1)*chunk_size)) for k in range(nChunks)]
-        if extra_chunk_size >0: chunks.append(list(range(n-n//nChunks, n)))
+        if extra_chunk_size >0: chunks.append(list(range(n-extra_chunk_size, n)))
         Hs = p.map(djikstra_wrapper, [(adj_mat_original, chunk) for chunk in chunks])
         H  = np.concatenate(Hs,0)
+        logging.info(f"Final Matrix {H.shape}")
     else:
         H = djisktra_wrapper(adj_mat_original, range(n))
         
