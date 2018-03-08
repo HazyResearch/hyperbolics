fdf813755dfd09f3e71c6f7549ecc4e1059762aa
fredsala <fredsala@stanford.edu>
2018-03-07 21:31:55 -0800

    Fix randomization bug Albert found

--- combinatorial/load_graph.py
+++ combinatorial/load_graph.py
@@ -4,12 +4,17 @@ import scipy as sp
 import numpy as np
 
 def load_graph(file_name):
-    G  = nx.read_edgelist(file_name)    
+    G  = nx.read_edgelist(file_name) 
+    
+    # work with integers as the node names    
+    G = nx.convert_node_labels_to_integers(G)
     
     # take the largest component
-    G_comp = max(nx.connected_component_subgraphs(G), key=len)
-    
-    # Work with integers as the node names
-    G_comp = nx.convert_node_labels_to_integers(G_comp)
+    G_comp_unsort = max(nx.connected_component_subgraphs(G), key=len)
 
+    # the connected_component function changes the edge orders, so fix:
+    G_comp_sorted = nx.Graph()
+    G_comp_sorted.add_edges_from(sorted(G_comp_unsort.edges()))
+    G_comp = nx.convert_node_labels_to_integers(G_comp_sorted)
+    
     return G_comp
