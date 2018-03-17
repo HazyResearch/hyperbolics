cbfbcde3d2701558df85d0a17cdd59c82b8e3226
Albert Gu <albertgu@stanford.edu>
2018-03-17 13:27:48 -0700

    parallel attempt 2

--- combinatorial/comb.jl
+++ combinatorial/comb.jl
@@ -1,54 +1,30 @@
-# using PyCall
-# using JLD
-# using ArgParse
-# using Pandas
-import JLD
-import ArgParse
-import Pandas
-import PyCall
-@everywhere using PyCall
-@everywhere using JLD
-@everywhere using ArgParse
-@everywhere using Pandas
-@everywhere @pyimport math as pymath
-@everywhere @pyimport networkx as nx
-@everywhere @pyimport scipy.sparse.csgraph as csg
-@everywhere @pyimport numpy as np
+using PyCall
+using JLD
+using ArgParse
+using Pandas
+# import JLD
+# import ArgParse
+# import Pandas
+# import PyCall
+# @everywhere using PyCall
+# @everywhere using JLD
+# @everywhere using ArgParse
+# @everywhere using Pandas
+@pyimport math as pymath
+@pyimport networkx as nx
+@pyimport scipy.sparse.csgraph as csg
+@pyimport numpy as np
 
-@everywhere unshift!(PyVector(pyimport("sys")["path"]), "")
+unshift!(PyVector(pyimport("sys")["path"]), "")
 # unshift!(PyVector(pyimport("sys")["path"]), "..")
-@everywhere unshift!(PyVector(pyimport("sys")["path"]), "combinatorial")
-@everywhere @pyimport utils.load_graph as lg
-@everywhere @pyimport distortions as dis
-@everywhere @pyimport graph_util as gu
+unshift!(PyVector(pyimport("sys")["path"]), "combinatorial")
+@pyimport utils.load_graph as lg
+@pyimport distortions as dis
+@pyimport graph_util as gu
 # push!(LOAD_PATH, "combinatorial")
 include("utilities.jl")
 include("rdim.jl")
 
-@everywhere py_map_row = dis.map_row 
-@everywhere py_distortion_row = dis.distortion_row 
-
-@everywhere function wrap_map_row(x,y,z,w)
-    return py_map_row(x,y,z,w) 
-end
-@everywhere function wrap_distortion_row(x,y,z,w)
-    return py_distortion_row(x,y,z,w) 
-end
-
-@everywhere function dist(u,v)
-    z  = 2*norm(u-v)^2
-    uu = 1 - norm(u)^2
-    vv = 1 - norm(v)^2
-    return acosh(1+z/(uu*vv))
-end
-@everywhere function dist_matrix_row(T,i)
-   (n,_) = size(T)
-   D = zeros(BigFloat,1,n)
-   for j in 1:n
-       D[1,j] = dist(T[i,:], T[j,:])
-   end
-   return D
-end
 
 # Parse command line arguments
 function parse_commandline()
@@ -89,6 +65,9 @@ function parse_commandline()
         "--auto-tau-float", "-a"
             help = "Calculate scale assuming 64-bit final embedding"
             action = :store_true
+        "--procs", "-y"
+            help = "number of processors to use"
+            arg_type = Int32
     end
     return parse_args(s)
 end
@@ -114,16 +93,13 @@ else
     println("Save embedding to $(parsed_args["embedding-save"])")
 end
 
-# http://julia-programming-language.2336112.n4.nabble.com/copy-a-local-variable-to-all-parallel-workers-td28722.html
-let GG        = lg.load_graph(parsed_args["dataset"])
-@everywhere G = $GG
-end
+G        = lg.load_graph(parsed_args["dataset"])
 weighted = gu.is_weighted(G)
 epsilon      = parsed_args["eps"]
 println("\nGraph information")
 
 # Number of vertices:
-@everywhere n = G[:order]();
+n = G[:order]();
 println("Number of vertices = $(n)");
 
 # Number of edges
@@ -133,16 +109,16 @@ println("Number of edges = $(num_edges)");
 # We'll use the BFS tree here - if G is already a tree
 #  this won't change anything, but if it's not we'll get a tree
 #  Also lets us use G.successors, parent, etc. function
-@everywhere root, d_max   = gu.max_degree(G)
-@everywhere G_BFS   = gu.get_BFS_tree(G, root)
+root, d_max   = gu.max_degree(G)
+G_BFS   = gu.get_BFS_tree(G, root)
 
 # A few statistics
-@everywhere n_bfs   = G_BFS[:order]();
+n_bfs   = G_BFS[:order]();
 degrees = G_BFS[:degree]();
 println("Max degree = $(d_max)")
 
 # Adjacency matrices for original graph and for BFS
-@everywhere adj_mat_original    = nx.to_scipy_sparse_matrix(G, 0:n-1)
+adj_mat_original    = nx.to_scipy_sparse_matrix(G, 0:n-1)
 adj_mat_bfs         = nx.to_scipy_sparse_matrix(G_BFS, 0:n_bfs-1)
 
 # Perform the embedding
@@ -158,10 +134,6 @@ elseif parsed_args["auto-tau-float"]
     tau = big(m/(1.3*path_length))
 else
     tau = get_emb_par(G_BFS, 1, epsilon, weighted)
-end
-
-let _tau        = tau
-@everywhere tau = $_tau
 end
 
 # Print out the scaling factor we got
@@ -183,10 +155,6 @@ else
 end
 toc()
 
-let _T = T
-@everywhere T = $_T
-end
-
 # Save the embedding:
 if parsed_args["embedding-save"] != nothing
     JLD.save(string(parsed_args["embedding-save"],".jld"), "T", T);
@@ -196,7 +164,8 @@ if parsed_args["embedding-save"] != nothing
     to_csv(df, parsed_args["embedding-save"])
 end
 
-if parsed_args["get-stats"]
+
+# if parsed_args["get-stats"]
     tic()
     println("\nComputing quality statistics")
     # The rest is statistics: MAP, distortion
@@ -209,13 +178,72 @@ if parsed_args["get-stats"]
         samples = min(parsed_args["stats-sample"], n_bfs)
         println("Using $samples sample rows for statistics")
     else
-        @everywhere samples = n_bfs
+        samples = n_bfs
     end
-    @everywhere sample_nodes = randperm(n_bfs)[1:samples]
+    sample_nodes = randperm(n_bfs)[1:samples]
 
     _maps   = zeros(samples)
     _d_avgs = zeros(samples)
     _wcs    = zeros(samples)
+
+
+    wrap_map_row = dis.map_row
+    wrap_distortion_row = dis.distortion_row
+    function wrap_csg_dijkstra(y,z,w)
+        return csg.dijkstra(adj_mat_original,indices=y,unweighted=z,directed=w)
+    end
+
+    addprocs(1)
+    println("nprocs $(nprocs())")
+@everywhere function py_map_row(x,y,z,w)
+    return wrap_map_row(x,y,z,w)
+end
+@everywhere function py_distortion_row(x,y,z,w)
+    return wrap_distortion_row(x,y,z,w)
+end
+# @everywhere function py_csg_dijkstra(y,z,w)
+#     return wrap_csg_dijkstra(y,z,w)
+# end
+@eval @everywhere py_csg_dijkstra = $wrap_csg_dijkstra
+
+@everywhere function dist(u,v)
+    z  = 2*norm(u-v)^2
+    uu = 1 - norm(u)^2
+    vv = 1 - norm(v)^2
+    return acosh(1+z/(uu*vv))
+end
+@everywhere function dist_matrix_row(T,i)
+   (n,_) = size(T)
+   D = zeros(BigFloat,1,n)
+   for j in 1:n
+       D[1,j] = dist(T[i,:], T[j,:])
+   end
+   return D
+end
+
+    # things that every worker should see
+    # http://julia-programming-language.2336112.n4.nabble.com/copy-a-local-variable-to-all-parallel-workers-td28722.html
+    # let
+    #     _n_bfs = n_bfs
+    #     _sample_nodes = sample_nodes
+    #     _tau = tau
+    #     _weighted = weighted
+    #     _adj_mat_original = adj_mat_original
+    #     @everywhere n_bfs = $_n_bfs
+    #     @everywhere sample_nodes = $_sample_nodes
+    #     @everywhere tau = $_tau
+    #     @everywhere weighted = $_weighted
+    #     @everywhere adj_mat_original = $_adj_mat_original
+    # end
+@eval @everywhere n_bfs = $(n_bfs)
+@eval @everywhere sample_nodes = $(sample_nodes)
+@eval @everywhere tau = $(tau)
+@eval @everywhere weighted = $(weighted)
+println("fat")
+# @eval @everywhere _adj_mat_original = $(adj_mat_original)
+
+
+println("fat")
 
     # Threads.@threads for i=1:samples
 
@@ -223,7 +251,8 @@ if parsed_args["get-stats"]
     @everywhere function compute_row_stats(i)
         # the real distances in the graph
         # true_dist_row = vec(csg.dijkstra(adj_mat_original, indices=[sample_nodes[i]-1], unweighted=(!weighted), directed=false))
-        true_dist_row = vec(csg.dijkstra(adj_mat_original, indices=[sample_nodes[i]-1], unweighted=true, directed=false))
+        # true_dist_row = vec(py_csg_dijkstra(_adj_mat_original, [sample_nodes[i]-1], true, false))
+        true_dist_row = vec(py_csg_dijkstra([sample_nodes[i]-1], true, false))
 
         # the hyperbolic distances for the points we've embedded
         hyp_dist_row = convert(Array{Float64},vec(dist_matrix_row(T, sample_nodes[i])/tau))
@@ -231,7 +260,7 @@ if parsed_args["get-stats"]
         # this is this row MAP
         # TODO: should this be n_bfs instead of n? n might include points that weren't embedded?
         # curr_map  = dis.map_row(true_dist_row, hyp_dist_row[1:n], n_bfs, sample_nodes[i]-1)
-        curr_map  = wrap_map_row(true_dist_row, hyp_dist_row[1:n], n_bfs, sample_nodes[i]-1)
+        curr_map  = py_map_row(true_dist_row, hyp_dist_row[1:n_bfs], n_bfs, sample_nodes[i]-1)
         # _maps[i]  = curr_map
 
         # print out current and running average MAP
@@ -241,7 +270,7 @@ if parsed_args["get-stats"]
 
         # these are distortions: worst cases (contraction, expansion) and average
         # mc, me, avg, bad = dis.distortion_row(true_dist_row, hyp_dist_row[1:n] ,n,sample_nodes[i]-1)
-        mc, me, avg, bad = wrap_distortion_row(true_dist_row, hyp_dist_row[1:n] ,n,sample_nodes[i]-1)
+        mc, me, avg, bad = py_distortion_row(true_dist_row, hyp_dist_row[1:n_bfs] ,n_bfs,sample_nodes[i]-1)
         # _wcs[i]  = mc*me
 
         # _d_avgs[i] = avg
@@ -249,11 +278,13 @@ if parsed_args["get-stats"]
     end
     # reduction function
     f = (a,b) -> (a[1]+b[1], a[2]+b[2], max(a[3],b[3]))
-    # (maps, d_avg, wc) = @parallel f for i=1:samples
-    #     compute_row_stats(i)
-    # end
+    (maps, d_avg, wc) = @sync @parallel f for i=1:samples
+        compute_row_stats(i)
+    end
     # Clean up
-    (maps, d_avg, wc) = reduce(f, pmap(compute_row_stats, 1:samples))
+    # row_stats = pmap(compute_row_stats, 1:samples)
+    # println("Row stats: $(row_stats)")
+    # (maps, d_avg, wc) = reduce(f, row_stats)
     # maps  = sum(_maps)
     # d_avg = sum(_d_avgs)
     # wc    = maximum(_wcs)
@@ -268,4 +299,4 @@ if parsed_args["get-stats"]
     println("Sum MAP = $(maps)")
     println("Sum d_avg = $(d_avg)")
     toc()
-end
+# end
