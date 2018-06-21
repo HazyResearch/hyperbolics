15253c3e46ec2ce2486bdac59d30236f9c57d568
Albert Gu <albertgu@stanford.edu>
2018-06-21 13:59:53 -0700

    ancestor tests

--- data/edges/ancestor_tests.py
+++ data/edges/ancestor_tests.py
@@ -1,16 +1,18 @@
 import networkx as nx
 
-def make_ancestor_closure(G, name):
+def make_ancestor_closure(G, name=None):
     G_BFS = nx.bfs_tree(G, 0)
     G_A   = nx.Graph()
-    f = open(name + ".edges", 'w')
+    if name is not None:
+        f = open(name + ".edges", 'w')
 
     for node in G_BFS.nodes():
         curr = node
         while len(list(G_BFS.predecessors(curr))):
             curr = list(G_BFS.predecessors(curr))[0]
             G_A.add_edge(node, curr)
-            f.write(str(node) + "\t" + str(curr) + "\n")
+            if name is not None:
+                f.write(str(node) + "\t" + str(curr) + "\n")
     f.close()
     return G_A
 
@@ -25,8 +27,9 @@ def save_edges(G, name, data=False):
 
     f.close()
 
-def make_tree_weights(G):
-    G_BFS = nx.bfs_tree(G, 0)
+def make_tree_weights(G, name=None):
+    G_DFS = nx.dfs_tree(G, 0)
+    G_BFS = nx.bfs_tree(G_DFS, 0)
     G_W   = nx.Graph()
 
     curr_nodes = [0]
@@ -49,10 +52,14 @@ def make_tree_weights(G):
         curr_nodes.remove(node)
         next_nodes += list(G_BFS.successors(node))
 
-    save_edges(G_W, "weighted_testanc", data=True)
+    if name is not None:
+        save_edges(G_W, name, data=True)
+
+    return G_W
 
 if __name__ == '__main__':
     G = nx.balanced_tree(2,3)
+    G.add_edge(9,10)
     make_ancestor_closure(G, 'testclosure')
-    make_tree_weights(G)
+    make_tree_weights(G, 'weighted_testanc')
     nx.write_edgelist(G, 'test.edges', data=False)
