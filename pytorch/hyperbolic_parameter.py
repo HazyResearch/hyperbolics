10f0f58c7910327f920d06cd52901accd52354da
Albert Gu <albertgu@stanford.edu>
2018-08-18 17:16:20 -0700

    remove trailing whitespace

--- pytorch/hyperbolic_parameter.py
+++ pytorch/hyperbolic_parameter.py
@@ -21,12 +21,12 @@ class Hyperbolic_Parameter(nn.Parameter):
         self.data    = x
 
     def modify_grad_inplace(self):
-        d        = self.data.dim() 
+        d        = self.data.dim()
         w_norm   = torch.norm(self.data,2,d - 1, True)
         # This is the inverse of the remanian metric, which we need to correct for.
         hyper_b  = (1 - w_norm**2)**2/4
         new_size = tuple([1] * (d - 1) + [self.data.size(d-1)])
-                    
+
         self.grad.data   *= hyper_b.repeat(*new_size) # multiply pointwise
         self.grad.data.clamp_(min=-10000.0, max=10000.0)
 
@@ -47,18 +47,18 @@ class Hyperbolic_Parameter(nn.Parameter):
         #new_size      = [1]*current_norms.dim() + [x.size(x.dim()-1)]
         #return modified.unsqueeze(modified.dim()).repeat(*new_size)
         return modified.unsqueeze(modified.dim()).expand(x.size())
-    
-    @staticmethod        
+
+    @staticmethod
     def _proj(x, eps=1e-10):
         return x * Hyperbolic_Parameter._correct(x, eps=eps)
 
     def proj(self, eps=1e-10):
         self.data *= Hyperbolic_Parameter._correct(self.data, eps=eps)
-        
+
     @staticmethod
     def correct_metric(ps):
         for p in ps:
-            if isinstance(p,Hyperbolic_Parameter): 
+            if isinstance(p,Hyperbolic_Parameter):
                 p.modify_grad_inplace()
 
     def __repr__(self):
