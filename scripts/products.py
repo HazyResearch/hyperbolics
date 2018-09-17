25a937d008444a3b36fbd9a613f1b187543b9092
Albert Gu <albertgu@stanford.edu>
2018-09-16 17:58:22 -0700

    add num gpus option to script

--- scripts/products.py
+++ scripts/products.py
@@ -55,16 +55,17 @@ models10 = [
     {'dim': 0, 'hyp': 0, 'edim': 0, 'euc': 0, 'sdim': 2, 'sph': 5},
     # {'dim': 0, 'hyp': 0, 'edim': 0, 'euc': 0, 'sdim': 1, 'sph': 20},
     {'dim': 5, 'hyp': 1, 'edim': 0, 'euc': 0, 'sdim': 5, 'sph': 1},
+    {'dim': 2, 'hyp': 5, 'edim': 0, 'euc': 0, 'sdim': 2, 'sph': 5},
     {'dim': 2, 'hyp': 2, 'edim': 3, 'euc': 1, 'sdim': 2, 'sph': 2}
 ]
 models = models10
 
 # lrs = [30, 100, 300]
-lrs = [100]
+lrs = [10, 20, 40]
 
 # CUDA_VISIBLE_DEVICES=1 python pytorch/pytorch_hyperbolic.py learn data/edges/synthetic/sierp-C50-2.edges --batch-size 65536 -d 50 --hyp 0 --euc 0 --edim 50 --sph 1 --sdim 51 -l 100.0 --epochs 1000 --checkpoint-freq 100 --resample-freq 500 -g --subsample 1024 --riemann --log-name C50-2.S50.log
 
-def run_pytorch(run_name, epochs, batch_size):
+def run_pytorch(run_name, gpus, epochs, batch_size):
     params = []
     # with open(f"{run_name}/pytorch.params", "w") as param_file:
     #     param_file.writelines("\n".join(params))
@@ -95,21 +96,19 @@ def run_pytorch(run_name, epochs, batch_size):
             '--riemann',
             '--learn-scale',
             # '--logloss',
-            # '--distortion',
+            # '--distloss',
+            # '--squareloss',
+            # '--symloss',
+            '--momentum', '0.9',
             '--learning-rate', str(lr)]
-        # if warm_start:
-        #     param += ['--warm-start', f"{run_name}/comb_embeddings/{dataset}.r{rank}.p{precision}.emb"]
         params.append(" ".join(param))
 
-    cmd0 = " ".join([ 'CUDA_VISIBLE_DEVICES=0', 'python', 'pytorch/pytorch_hyperbolic.py', 'learn' ])
-    cmd1 = " ".join([ 'CUDA_VISIBLE_DEVICES=1', 'python', 'pytorch/pytorch_hyperbolic.py', 'learn' ])
-
-    cmds0 = [f'{cmd0} {p}' for p in params[0::2]]
-    cmds1 = [f'{cmd1} {p}' for p in params[1::2]]
-    with open(f"{run_name}/cmds0.sh", "w") as cmd_log:
-        cmd_log.writelines('\n'.join(cmds0))
-    with open(f"{run_name}/cmds1.sh", "w") as cmd_log:
-        cmd_log.writelines('\n'.join(cmds1))
+    cmds = []
+    for i in range(gpus):
+        header = " ".join([ 'CUDA_VISIBLE_DEVICES='+str(i), 'python', 'pytorch/pytorch_hyperbolic.py', 'learn' ])
+        cmds = [f'{header} {p}' for p in params[i::gpus]]
+        with open(f"{run_name}/cmds{i}.sh", "w") as cmd_log:
+            cmd_log.writelines('\n'.join(cmds))
 
     # all_cmds = [f'"{cmd0} {p}"' for p in params[0::2]] \
     # + [f'"{cmd1} {p}"' for p in params[1::2]]
@@ -124,13 +123,14 @@ def run_pytorch(run_name, epochs, batch_size):
 
 
 @argh.arg("run_name", help="Directory to store the run; will be created if necessary")
+@argh.arg("--gpus", help="Number of GPUs to use")
 # @argh.arg('-d', "--datasets", nargs='+', type=str, help = "Datasets")
 @argh.arg("--epochs", help="Number of epochs to run Pytorch optimizer")
 @argh.arg("--batch-size", help="Batch size")
-def run(run_name, epochs=3000, batch_size=65536):
+def run(run_name, gpus=1, epochs=2000, batch_size=65536):
     os.makedirs(run_name, exist_ok=True)
 
-    run_pytorch(run_name, epochs=epochs, batch_size=batch_size)
+    run_pytorch(run_name, gpus=gpus, epochs=epochs, batch_size=batch_size)
 
 
 if __name__ == '__main__':
