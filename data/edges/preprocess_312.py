6673ad2f1ec81b8d8be706f66417d6512cc6f418
Albert Gu <albertgu@stanford.edu>
2018-09-26 21:39:27 -0700

    flights graph

---
+++ data/edges/preprocess_312.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+filename = 'usca312.txt'
+fileout = 'usca312.edges'
+if __name__ == '__main__':
+    D = np.loadtxt(filename)
+    print(D.shape)
+    n = D.shape[0]
+    with open(fileout, 'w') as fout:
+        for i in range(n):
+            for j in range(i+1,n):
+                e = np.minimum(D[i][j], D[j][i])
+                fout.write(f'{i} {j} {e/1000.}\n')
