de53c252cc604f3372897bd71f75d0f4ea2b2b13
Albert Gu <albertgu@stanford.edu>
2018-09-26 23:34:29 -0700

    add distance matrix preprocessing script

--- data/edges/preprocess_dist_matrix.py
+++ data/edges/preprocess_dist_matrix.py
@@ -1,8 +1,11 @@
 import numpy as np
+import os
 
 filename = 'usca312.txt'
-fileout = 'usca312.edges'
+# fileout = 'usca312.edges'
 if __name__ == '__main__':
+    base, ext = os.path.splitext(filename)
+    fileout = f'{base}.edges'
     D = np.loadtxt(filename)
     print(D.shape)
     n = D.shape[0]
