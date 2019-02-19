ef3c5226a573c765879cdfb4a9517a1975cf8d16
fredsala <fredsala@stanford.edu>
2019-02-19 14:25:34 -0800

    Revert "Implement batching, dataloaders"
    This reverts commit 670c550d6ef51f397b29e754301e7f3e48c57c24.

--- utils/mapping_utils.py
+++ utils/mapping_utils.py
@@ -10,7 +10,6 @@ import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
 import torch.optim as optim
 import time
 import math
@@ -45,19 +44,6 @@ def dist_h(u,v):
     uu = 1. + torch.div(z,((1-torch.norm(u,2)**2)*(1-torch.norm(v,2)**2)))
     return acosh(uu)
 
-def dist_p(u,v):
-    z  = 2*torch.norm(u-v,2)**2
-    uu = 1. + torch.div(z,((1-torch.norm(u,2)**2)*(1-torch.norm(v,2)**2)))
-    machine_eps = np.finfo(uu.data.detach().cpu().numpy().dtype).eps  # problem with cuda tensor
-    return acosh(torch.clamp(uu, min=1+machine_eps))
-
-def dist_pb(u,v):
-
-    z  = 2*torch.norm(u-v,2, dim=1)**2
-    uu = 1. + torch.div(z,((1-torch.norm(u,2, dim=1)**2)*(1-torch.norm(v,2, dim=1)**2)))
-    machine_eps = np.finfo(uu.data.detach().cpu().numpy().dtype).eps  # problem with cuda tensor
-    return acosh(torch.clamp(uu, min=1+machine_eps))
-
 def distance_matrix_euclidean(input):
     row_n = input.shape[0]
     mp1 = torch.stack([input]*row_n)
@@ -65,20 +51,15 @@ def distance_matrix_euclidean(input):
     dist_mat = torch.sum((mp1-mp2)**2,2).squeeze()
     return dist_mat
 
-def distance_matrix_hyperbolic(input, sampled_rows, scale):
-    #print("were computing the matrix with sampled_rows = ")
-    #print(sampled_rows)
-    batch_size = input.shape[0]
-    row_n = input.shape[1]
-    dist_mat = torch.zeros(batch_size, len(sampled_rows), row_n, device=device)
+def distance_matrix_hyperbolic(input):
+    row_n = input.shape[0]
+    dist_mat = torch.zeros(row_n, row_n, device=device)
     # num_cores = multiprocessing.cpu_count()
     # dist_mat = Parallel(n_jobs=num_cores)(delayed(compute_row)(i,adj_mat) for i in range(n))
-    idx = 0
-    for row in sampled_rows:
+    for row in range(row_n):
         for i in range(row_n):
             if i != row:
-                dist_mat[:,idx, i] = dist_pb(input[:,row,:], input[:,i,:])*scale
-        idx += 1
+                dist_mat[row, i] = dist_h(input[row,:], input[i,:])
     # print("Distance matrix", dist_mat)
     return dist_mat
 
@@ -102,45 +83,12 @@ def distortion_row(H1, H2, n, row):
     # print("Number of good entries", good)
     return (avg, good)
 
-def distortion(H1, H2, n, sampled_rows, jobs=16):
+def distortion(H1, H2, n, jobs=16):
     # dists = Parallel(n_jobs=jobs)(delayed(distortion_row)(H1[i,:],H2[i,:],n,i) for i in range(n))
-    # print(H1.shape) #target
-    # print(H2.shape) #recovered
-    batch_size = H1.shape[0]
-    dists = torch.zeros(batch_size, len(sampled_rows))
-    for b in range(batch_size):
-        i=0
-        for row in sampled_rows:
-        #     print(H1[b,i,:].shape)
-        #     print(H2[b,row,:].shape)
-            dists[b,i] = distortion_row(H1[b,row,:], H2[b,i,:], n, row)[0]
-            i += 1
-
-    #to_stack = [tup[0] for tup in dists]
-    #avg = torch.stack(to_stack).sum() / len(sampled_rows)
-    avg = dists.sum(dim=1)/len(sampled_rows)
-    avg = avg.sum()/batch_size
-    return avg
-
-    
-'''
-
-def distortion(H1, H2, n, jobs):
-    H1 = np.array(H1.cpu()), 
-    H2 = np.array(H2.detach().cpu())
-    dists = Parallel(n_jobs=jobs)(delayed(distortion_row)(H1[i,:],H2[i,:],n,i) for i in range(n))
-    dists = np.vstack(dists)
-    mc = max(dists[:,0])
-    me = max(dists[:,1])
-    # wc = max(dists[:,0])*max(dists[:,1])
-    avg = sum(dists[:,2])/n
-    bad = sum(dists[:,3])
-    #return (mc, me, avg, bad)    
+    dists = (distortion_row(H1[i,:],H2[i,:],n,i) for i in range(n))
     to_stack = [tup[0] for tup in dists]
     avg = torch.stack(to_stack).sum()/n
     return avg
-'''
-
 
 #Loading the graph and getting the distance matrix.
 
@@ -202,49 +150,27 @@ def pairfromidx(idx, edge_folder):
     n = G.order()
     return ([], target_tensor, n, G)
 
-def gettestpairs(edge_folder, test_folder):
+def gettestpairs(test_folder):
     test_pairs = defaultdict()
-    edge_files = os.listdir(edge_folder)
+    edge_files = os.listdir(test_folder+"edges/")
     for file in edge_files:
         name = file.split("/")[-1]
-        ground_truth = load_graph(edge_folder+file)
+        ground_truth = load_graph(test_folder+"edges/"+file)
         n = ground_truth.order()
-        md = torch.load(test_folder+"emb/"+str(name)+".E10-1.lr10.0.emb.final", map_location=device)
-        embedding = md.E[0].w
-        norm = embedding.norm(p=2, dim=1, keepdim=True)
-        max_norm = torch.max(norm)+1e-10
-        normalized_emb = embedding.div(max_norm.expand_as(embedding))
+        euclidean_emb = torch.load(test_folder+"emb_tensor/"+str(name)+".E10-1.lr10.0.emb.final", map_location=torch.device('cpu'))
         target_matrix = get_dist_mat(ground_truth)
         target_tensor = torch.from_numpy(target_matrix).float().to(device)
         target_tensor.requires_grad = False
-        test_pairs[name] = [normalized_emb, ground_truth, target_tensor, n]
+        test_pairs[name] = [euclidean_emb, ground_truth, target_tensor, n]
     return test_pairs
 
 def compare_mst(G, hrec):
-
     mst = csg.minimum_spanning_tree(hrec)
     G_rec = nx.from_scipy_sparse_matrix(mst)
+
     found = 0
     for edge in G_rec.edges():
         if edge in G.edges(): found+= 1
 
     acc = found / len(list(G.edges()))
     return acc
-
-def compare_mst_batch(target_batch, hrec_batch):
-
-    batch_size = hrec_batch.shape[0]
-    batch_acc = 0
-    for i in range(batch_size):
-        hrec = hrec_batch[i,:,:]
-        target = target_batch[i,:,:]
-        mst = csg.minimum_spanning_tree(hrec)
-        G_rec = nx.from_scipy_sparse_matrix(mst)
-        G = nx.from_numpy_matrix(target)
-        found = 0
-        for edge in G_rec.edges():
-            if edge in G.edges(): found+= 1
-
-        acc = found / len(list(G.edges()))
-        batch_acc += acc
-    return batch_acc/batch_size
